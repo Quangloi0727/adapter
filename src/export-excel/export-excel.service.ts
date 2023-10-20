7d55ce9f322c665262827c48835c8c63485daf6b
loinq <loinq@metechvn.com>
2023-10-20 17:43:36 +0700

    update

--- src/export-excel/export-excel.service.ts
+++ src/export-excel/export-excel.service.ts
@@ -46,8 +46,8 @@ export class ExportExcelService {
 
     await this._sftpService.mkdirIfNotExists(path.join(getDayMonthYear().year, getDayMonthYear().month, getDayMonthYear().day, 'recording'));
 
-    const filePath = `${dir}/${getDayMonthYear().valueOf}.csv`;
+    const filePath = `${path.join(dir, getDayMonthYear().valueOf.toString())}.csv`;
     return workbook.xlsx.writeFile(filePath);
   }
 
-}
+};
